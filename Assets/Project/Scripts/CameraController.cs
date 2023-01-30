e6b7ff2c1fb466ecdbeef951ea4462770d079c4a
gotdroidgames <gotdroidgames@gmail.com>
2023-01-30 14:51:53 +0300

    Menu UI

--- Assets/Project/Scripts/CameraController.cs
+++ Assets/Project/Scripts/CameraController.cs
@@ -20,6 +20,7 @@ public class CameraController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             doMovement = !doMovement;
+            AudioManager.Instance.PlaySFX("Die");
         }
 
         if(!doMovement)
---
+++ Assets/Project/Scripts/Menu/MenuUI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor.Experimental.RestService;
+using Newtonsoft.Json.Linq;
+using Unity.VisualScripting;
+
+public class MenuUI : MonoBehaviour
+{
+    [Header("Button System") ]
+    [SerializeField] GameObject playButton;
+    [SerializeField] GameObject optionsButton;
+    [SerializeField] GameObject QuitButton;
+    [SerializeField] GameObject _backButton;
+    [SerializeField] GameObject _musicButton,_sfxButton;
+    [SerializeField] Slider _musicSlider, _sfxSlider;
+
+
+
+    private void Awake()
+    {
+        _backButton.SetActive(false);
+        _musicButton.SetActive(false);
+        _sfxButton.SetActive(false);
+    }
+
+    public void ToggleMusic()
+    {
+        AudioManager.Instance.ToggleMusic();
+    }
+
+    public void ToggleSFX()
+    {
+        AudioManager.Instance.ToggleSFX();
+    }
+
+    public void MusicVolume()
+    {
+        AudioManager.Instance.MusicVolume(_musicSlider.value);
+    }
+
+    public void SFXVolume()
+    {
+        AudioManager.Instance.SFXVolume(_sfxSlider.value);
+    }
+
+    public void BackButton()
+    {
+        playButton.SetActive(true);
+        optionsButton.SetActive(true);
+        QuitButton.SetActive(true);
+        _backButton.SetActive(false);
+        _sfxButton.SetActive(false);
+        _musicButton.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OptionsButton()
+    {
+        playButton.SetActive(false);
+        optionsButton.SetActive(false);
+        QuitButton.SetActive(false);
+        _backButton.SetActive(true);
+        _sfxButton.SetActive(true);
+        _musicButton.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
