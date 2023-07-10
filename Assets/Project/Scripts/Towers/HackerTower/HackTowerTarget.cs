7e7a26269c9338e7e1366d5ebcb72903814e845a
gotdroidgames <gotdroidgames@gmail.com>
2023-07-10 11:42:07 +0300

    Not Important

--- Assets/Project/Scripts/Towers/HackerTower/HackTowerTarget.cs
+++ Assets/Project/Scripts/Towers/HackerTower/HackTowerTarget.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HackTowerRange : MonoBehaviour
+public class HackTowerTarget : MonoBehaviour
 {
     HackerTowerMenu hackmenu;
     GameManager gamemanager;
