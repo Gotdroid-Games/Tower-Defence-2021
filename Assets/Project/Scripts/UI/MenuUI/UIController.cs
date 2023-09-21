83c67c9f05192d228e93f89bcad382ed6ace8817
Mehmet Emre <emrekv3434@gmail.com>
2023-09-21 19:33:36 +0300

    starpoint system updated

--- Assets/Project/Scripts/UI/MenuUI/UIController.cs
+++ Assets/Project/Scripts/UI/MenuUI/UIController.cs
@@ -5,11 +5,24 @@ using TMPro;
 public class UIController : MonoBehaviour
 {
     public int StarMenuUI;
+    public int totalStarPoints = 0;
     public TextMeshProUGUI StarMenuText;
 
+    private void Start()
+    {
+        totalStarPoints += StarPoint._starPoint;
+        UpdateStarPoints();
+    }
     private void Update()
     {
-        StarMenuUI = StarPoint._starPoint;
-        StarMenuText.text=StarMenuUI.ToString();
+
+        
+        //  StarMenuUI = StarPoint._starPoint;
+        StarMenuText.text = totalStarPoints.ToString();
+
+    }
+    private void UpdateStarPoints()
+    {
+        StarMenuText.text = totalStarPoints.ToString();
     }
 }
