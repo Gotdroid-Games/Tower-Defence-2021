76219d4fed47198003e35ae4d5df4b6dc3ff21da
Mehmet Emre <emrekv3434@gmail.com>
2023-09-25 01:07:50 +0300

    StarPoint bug was fixed

--- Assets/Project/Scripts/UI/MenuUI/UIController.cs
+++ Assets/Project/Scripts/UI/MenuUI/UIController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-
+using System.IO;
+[System.Serializable]
+public class TotalStarPoints
+{
+    public int totalstarpoints;
+}
 public class UIController : MonoBehaviour
 {
     public int StarMenuUI;
@@ -10,6 +15,7 @@ public class UIController : MonoBehaviour
 
     private void Start()
     {
+        LoadTotalPoints();
         totalStarPoints += StarPoint._starPoint;
         UpdateStarPoints();
     }
@@ -19,10 +25,28 @@ public class UIController : MonoBehaviour
         
         //  StarMenuUI = StarPoint._starPoint;
         StarMenuText.text = totalStarPoints.ToString();
+        SaveTotalPoints();
 
     }
     private void UpdateStarPoints()
     {
         StarMenuText.text = totalStarPoints.ToString();
+    }
+    private void SaveTotalPoints()
+    {
+        TotalStarPoints data = new TotalStarPoints();
+        data.totalstarpoints = totalStarPoints;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.dataPath + "/totalStarPoints.json", json);
+    }
+
+    private void LoadTotalPoints()
+    {
+        if (File.Exists(Application.dataPath + "/totalStarPoints.json"))
+        {
+            string json = File.ReadAllText(Application.dataPath + "/totalStarPoints.json");
+            TotalStarPoints data = JsonUtility.FromJson<TotalStarPoints>(json);
+            totalStarPoints = data.totalstarpoints;
+        }
     }
 }
